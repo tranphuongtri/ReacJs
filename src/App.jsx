9f2e1598855961ee10a8f81f07edf0825df382fb
tranphuongtri <gunnybboss@gmail.com>
2025-02-19 11:03:13 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  
+  function handleChange(e){
+   setName(e.target.value);
+  }
+  function handleClick(){
+    // console.log(name);
+    setText(name);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <input placeholder='Input Name' onChange={handleChange} type="text" name="" id="" />
+    <br />
+    <button>Click{handleClick}</button>
+    <p>Hello: {name}</p>
     </>
   )
 }
